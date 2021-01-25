ecd5339b5dd0a265b876ee44979c3ae8fa45613f
yoRyuuuuu <sasoseooo@gmail.com>
2021-01-25 11:21:42 +0900

    エラー処理を削除．next_token()をpublicに変更

--- src/lexer.rs
+++ src/lexer.rs
@@ -23,59 +23,58 @@ impl<'a> Lexer<'a> {
         lexer
     }
 
-    pub fn lex(&mut self) -> Result<Vec<Token>> {
-        use Token::*;
+    pub fn lex(&mut self) -> Vec<Token> {
         let mut tokens = vec![];
         while self.ch != 0 {
-            let token = self.next_token()?;
+            let token = self.next_token();
             tokens.push(token);
         }
 
-        Ok(tokens)
+        tokens
     }
 
-    fn next_token(&mut self) -> Result<Token> {
+    pub fn next_token(&mut self) -> Token {
         use Token::*;
         let token = match self.ch {
             b'{' | b'}' | b'(' | b')' | b'[' | b']' | b'.' | b',' | b';' | b'+' | b'-' | b'*'
             | b'|' | b'=' | b'~' => {
                 let token = Symbol((self.ch as char).to_string());
                 self.consume_char();
-                Ok(token)
+                token
             }
             b'a'..=b'z' | b'A'..=b'Z' => {
                 let literal = self
                     .consume_many(|ch| (b'a'..=b'z').contains(&ch) | (b'A'..=b'Z').contains(&ch));
                 let token = look_up_table(literal);
-                Ok(token)
+                token
             }
             b'0'..=b'9' => {
                 let number = self.consume_many(|ch| b"0123456789".contains(&ch));
-                Ok(IntConst(number))
+                IntConst(number)
             }
             b'<' => {
                 let token = Symbol("&lt;".to_string());
                 self.consume_char();
-                Ok(token)
+                token
             }
             b'>' => {
                 let token = Symbol("&gt;".to_string());
                 self.consume_char();
-                Ok(token)
+                token
             }
             b'&' => {
                 let token = Symbol("&amp;".to_string());
                 self.consume_char();
-                Ok(token)
+                token
             }
             b'"' => {
                 self.consume_char();
                 let string = self.consume_many(|ch| ch != b'"');
                 self.consume_char();
-                Ok(StringConst(string))
+                StringConst(string)
             }
-            b'/' => Ok(self.consume_comment()),
-            _ => Err(anyhow!("invalid char {}", self.ch)),
+            b'/' => self.consume_comment(),
+            _ => Eof,
         };
 
         self.skip_white();
