67c93704fd4545219559b6dbe63702bf58640b7f
yoRyuuuuu <sasoseooo@gmail.com>
2021-01-25 11:30:21 +0900

    parse_expr(), parse_term()を追加

--- src/parser.rs
+++ src/parser.rs
@@ -1,37 +1,78 @@
+use std::unimplemented;
 
 use crate::ast::*;
+use crate::lexer::Lexer;
 use crate::token::Token;
 use anyhow::{anyhow, Result};
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-
-pub struct Parser {
-    tokens: Vec<Token>,
+#[derive(Debug, Clone)]
+pub struct Parser<'a> {
+    lexer: Lexer<'a>,
     cur_token: Token,
-    peek_token: Token,
 }
 
-impl Parser {
-    pub fn new(tokens: Vec<Token>) -> Self {
+impl<'a> Parser<'a> {
+    pub fn new(lexer: Lexer<'a>) -> Self {
         let mut parser = Self {
-            tokens,
+            lexer,
             cur_token: Token::Eof,
-            peek_token: Token::Eof,
         };
-        parser.next_token().unwrap();
-        parser.next_token().unwrap();
+        parser.next_token();
         parser
     }
 
-    fn next_token(&mut self) -> Result<()> {
-        self.cur_token = self.peek_token.to_owned();
-        self.peek_token = self
-            .tokens
-            .iter()
-            .next()
-            .ok_or(anyhow!("no token"))?
-            .to_owned();
-        Ok(())
+    fn next_token(&mut self) {
+        self.cur_token = self.lexer.next_token();
     }
 
+    pub fn parse_expr(&mut self) -> Result<Expression> {
+        let left = self.parse_term()?;
+        match self.cur_token {
+            Token::Symbol(ref literal) => {
+                let literal = literal.as_str();
+                let op = match literal {
+                    "+" => Operator::Plus,
+                    "-" => Operator::Minus,
+                    "*" => Operator::Aster,
+                    "/" => Operator::Slash,
+                    "&amp;" => Operator::And,
+                    "|" => Operator::Or,
+                    "&lt;" => Operator::LessThan,
+                    "&gt;" => Operator::GreaterThan,
+                    "=" => Operator::Assign,
+                    _ => {
+                        return Err(anyhow!(
+                            "unexpected symbol {:?} in parse_expr",
+                            self.cur_token
+                        ))
+                    }
+                };
+                self.next_token();
+                let right = Box::new(self.parse_expr()?);
+                Ok(Expression::Binary { left, op, right })
+            }
+            _ => Ok(Expression::Unary(left)),
+        }
+    }
+
+    pub fn parse_term(&mut self) -> Result<Term> {
+        let term = match self.cur_token {
+            Token::IntConst(ref literal) => Term::IntConst(literal.to_owned()),
+            Token::StringConst(ref literal) => Term::StringConst(literal.to_owned()),
+            Token::True => Term::True,
+            Token::False => Term::False,
+            Token::Null => Term::Null,
+            Token::This => Term::This,
+            Token::Ident(ref string) => {
+                unimplemented!();
+            },
+            _ => {
+                return Err(anyhow!(
+                    "unexpected token {:?} in parse_term",
+                    self.cur_token
+                ))
+            }
+        };
+        self.next_token();
+        Ok(term)
+    }
 }
